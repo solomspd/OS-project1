1dbb2d8255b00f8f0f74e625552cd269686b8ee5
Abdelsalam ElTamawy <solomspd@aucegypt.edu>
2020-03-28 04:17:33 +0200

    cleaned a lil thing

--- src/main.c
+++ src/main.c
@@ -1,17 +1,16 @@
 #include "FileTree.h"
 #include <stdio.h>
 
-
 const int mx_char = 255;
 
 
 int main(int argc, char **argv) {
 
-    FileTree X;
-    FileTree_init(&X,"./");
-    FileTree_calculateSizes(&X);
-    printf("%lli", X.root->size);
-    FileTree_destruct(&X);
+	FileTree X;
+	FileTree_init(&X,".");
+	//FileTree_calculateSizes(&X); Obsolete
+	printf("%lli", X.root->size);
+	FileTree_destruct(&X);
 
-    return 0;
+	return 0;
 }
